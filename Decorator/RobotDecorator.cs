f908a0c8213abba671dc07de9ebdf0e9923a3339
Kamil Ruta <4ruta4@gmail.com>
2018-04-20 23:37:21 +0200

    First upload

---
+++ Decorator/RobotDecorator.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots
+{
+    abstract class RobotDecorator : PaintingRobot
+    {
+        public string DescriptionTemplate(string name, PaintingRobot r2)
+        {
+            string result = r2.ToString();
+            result = result.Replace("\n", "\n\t");
+            return $"{name} {this.GetPaintedLength(1)}\n(\n\t{result}\n)";
+        }
+
+        public string DescriptionTemplate(string name, List<PaintingRobot> robots)
+        {
+            string result = "";
+            foreach (var robot in robots)
+            {
+                result += "\t" + robot.ToString().Replace("\n", "\n\t") + "\n";
+            }
+            return $"{name} {this.GetPaintedLength(1)}\n(\n{result})";
+        }
+    }
+
+    class RobotDecoratorAdd : RobotDecorator
+    {
+        private double n;
+        private PaintingRobot robot;
+
+        public RobotDecoratorAdd(PaintingRobot robot, double n)
+        {
+            this.robot = robot;
+            this.n = n;
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            return robot.GetPaintedLength(time) + n;
+        }
+
+        public override string ToString()
+        {
+            return this.DescriptionTemplate("adding robot", robot);
+        }
+    }
+
+    class RobotDecoratorMultiply : RobotDecorator
+    {
+        private double n;
+        private PaintingRobot robot;
+
+        public RobotDecoratorMultiply(PaintingRobot robot, double n)
+        {
+            this.robot = robot;
+            this.n = n;
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            return robot.GetPaintedLength(time) * n;
+        }
+
+        public override string ToString()
+        {
+            return this.DescriptionTemplate("multiplying robot", robot);
+        }
+    }
+
+    class RobotDecoratorNlogN : RobotDecorator
+    {
+        private PaintingRobot robot;
+
+        public RobotDecoratorNlogN(PaintingRobot robot)
+        {
+            this.robot = robot;
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            double n = robot.GetPaintedLength(time);
+            return n * Math.Log(n);
+        }
+
+        public override string ToString()
+        {
+            return this.DescriptionTemplate("NlogN robot", robot);
+        }
+    }
+
+    class RobotDecoratorMax : RobotDecorator
+    {
+        private List<PaintingRobot> robots;
+
+        public RobotDecoratorMax(List<PaintingRobot> robots)
+        {
+            this.robots = robots;
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            double max = 0;
+            foreach(var robot in robots)
+            {
+                if(robot.GetPaintedLength(time) > max)
+                {
+                    max = robot.GetPaintedLength(time);
+                }
+            }
+            return max;
+        }
+
+        public override string ToString()
+        {
+            return this.DescriptionTemplate("max robot", robots);
+        }
+    }
+
+    class RobotDecoratorSum : RobotDecorator
+    {
+        private List<PaintingRobot> robots;
+
+        public RobotDecoratorSum(List<PaintingRobot> robots)
+        {
+            this.robots = robots;
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            double sum = 0;
+            foreach (var robot in robots)
+            {
+                sum += robot.GetPaintedLength(time);
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            return this.DescriptionTemplate("sum robot", robots);
+        }
+    }
+
+    class LazyRobot : PaintingRobot
+    {
+        double n;
+
+        public LazyRobot(double n)
+        {
+            this.n = n;
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            return n * Math.Log(time);
+        }
+
+        public override string ToString()
+        {
+            return "lazy robot";
+        }
+    }
+}
---
+++ Iterator/PermutationSet.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PermutationSet
+{
+    public partial class PermutationSet
+    {
+        private List<int> basePermutation;
+        private Dictionary<int, int> translator;
+        private Node root;
+
+        private struct Transposition
+        {
+            public int i1;
+            public int i2;
+        }
+
+        private class Node
+        {
+            public List<Node> Children { get; }
+            public Transposition Value { get; }
+            public bool End { get; set; }
+
+            public Node(Transposition value)
+            {
+                Children = new List<Node>();
+                Value = value;
+            }
+        }
+
+        public IEnumerator<List<int>> GetEnumerator()
+        {
+            return new PermutationEnumerator(root, new List<int>(basePermutation));
+        }
+
+        private class PermutationEnumerator : IEnumerator<List<int>>
+        {
+            Node root;
+            List<int> permutation;
+            List<int> basePermutation;
+            List<int> result;
+            IEnumerator<List<int>> enumerator;
+            int s;
+
+            public PermutationEnumerator(Node root, List<int> basePermutation)
+            {
+                this.root = root;
+                this.basePermutation = basePermutation;
+                this.permutation = new List<int>(basePermutation);
+                s = -1;
+            }
+
+            public object Current => result;
+
+            List<int> IEnumerator<List<int>>.Current => result;
+
+            public void Dispose()
+            {
+                return;
+            }
+
+            public bool MoveNext()
+            {
+
+                if (s == -1)
+                {
+                    Switch(root.Value);
+                    result = permutation;
+                    s = 0;
+                    if (s < root.Children.Count)
+                        enumerator = new PermutationEnumerator(root.Children[s], new List<int> (permutation));
+                    if (!root.End)
+                    {
+                        return MoveNext();
+                    }
+                    else
+                        return true;
+                }
+                else if (s < root.Children.Count)
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        result = enumerator.Current;
+                        return true;
+                    }
+                    else
+                    {
+                        s++;
+                        if (s < root.Children.Count)
+                        {
+                            enumerator = new PermutationEnumerator(root.Children[s], new List<int>(permutation));      
+                        }
+                        return MoveNext();
+                    }
+                }
+                else
+                    return false;
+            }
+
+            public void Reset()
+            {
+                this.permutation = new List<int>(basePermutation);
+                s = -1;
+            }
+
+            private void Switch(Transposition trans)
+            {
+                int tmp = permutation[trans.i1 ];
+                permutation[trans.i1 ] = permutation[trans.i2 ];
+                permutation[trans.i2 ] = tmp;
+            }
+        }
+    }
+}
